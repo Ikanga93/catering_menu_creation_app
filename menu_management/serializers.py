c7201ec9d49a42068ffaa257be42a38ad4546faf
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-11-15 16:19:19 -0500

    Updated serializers.py to add email requirement for signing up

--- menu_management/serializers.py
+++ menu_management/serializers.py
@@ -15,21 +15,32 @@ class MenuSerializer(serializers.ModelSerializer):
 
 # UserRegistrationSerializer
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2']
+        fields = ['username', 'email', 'password', 'password2']
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("Passwords do not match.")
+        
+         # Ensure email is unique
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError({"email": "Email is already in use."})
+        
         return data
 
     def create(self, validated_data):
+        # Remove password2 as it's not needed for user creation
+        validated_data.pop('password2')
+
         user = User.objects.create_user(
             username=validated_data['username'],
+            email=validated_data['email'],
             password=validated_data['password']
         )
         return user
