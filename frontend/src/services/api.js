ffcfbb025cb98a2d63b8e0fe631b8774740fddb7
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-11-15 10:27:35 -0500

    Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api, http://127.0.0.1:8000', // Ensure this matches your backend URL
+    baseURL: 'http://127.0.0.1:8000/api', // Ensure this matches your backend URL
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
