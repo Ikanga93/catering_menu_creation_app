2368188362105f085a6fea38a03e336583b0edba
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-11-14 23:49:32 -0500

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
+// src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // No need to import BrowserRouter here
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Register from './components/Register';
@@ -9,49 +10,47 @@ import MenuList from './components/MenuList';
 import CreateMenu from './components/CreateMenu';
 import EditMenu from './components/EditMenu';
 import PrivateRoute from './components/PrivateRoute';
-import { AuthProvider } from './contexts/AuthContext'; // Import AuthProvider
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-
+// Remove unnecessary imports
+// import { AuthProvider } from './contexts/AuthContext'; // Not needed here
+// import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
-    <AuthProvider> {/* Wrap with AuthProvider */}
-      <Router>
-          <Navbar />
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route
-                  path="/menus"
-                  element={
-                      <PrivateRoute>
-                          <MenuList />
-                      </PrivateRoute>
-                  }
-              />
-              <Route
-                  path="/create-menu"
-                  element={
-                      <PrivateRoute>
-                          <CreateMenu />
-                      </PrivateRoute>
-                  }
-              />
-              <Route
-            path="/edit-menu/:id"
-            element={
-              <PrivateRoute>
-                <EditMenu />
-              </PrivateRoute>
-            }
-            />
-              {/* Add other routes here */}
-          </Routes>
-          <ToastContainer /> {/* Add ToastContainer for notifications */}
-      </Router>
-    </AuthProvider>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/menus"
+          element={
+            <PrivateRoute>
+              <MenuList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/create-menu"
+          element={
+            <PrivateRoute>
+              <CreateMenu />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/edit-menu/:id"
+          element={
+            <PrivateRoute>
+              <EditMenu />
+            </PrivateRoute>
+          }
+        />
+        {/* Add other routes here */}
+      </Routes>
+      {/* Remove ToastContainer from App.js since it's already in index.js */}
+    </>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,24 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Router>
+        <App />
+        <ToastContainer />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-      <AuthProvider>
-          <App />
-      </AuthProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
